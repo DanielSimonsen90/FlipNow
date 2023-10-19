d48d58bd0b849cb247798b0812e6aa56641f01ca
Daniel Simonsen <danielsimonsen90@gmail.com>
2023-10-19 13:35:49 +0200

    Create seed data

---
+++ Projects/Backend/FlipNow.DataAccess/SeedData/Users.cs
@@ -0,0 +1,21 @@
+﻿using FlipNow.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipNow.DataAccess.SeedData;
+
+public static class Users
+{
+    public static User[] SeedData = new[]
+    {
+        new User()
+        {
+            Username = "Dumbho",
+            AvgScore = 0,
+            HighestScore = new UserScore()
+        }
+    };
+}
