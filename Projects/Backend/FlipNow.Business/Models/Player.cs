2f29491f48e61758f2da0d0180d22e794015b67d
Daniel Simonsen <danielsimonsen90@gmail.com>
2023-10-19 14:21:32 +0200

    Create Player.cs

---
+++ Projects/Backend/FlipNow.Business/Models/Player.cs
@@ -0,0 +1,20 @@
+﻿using FlipNow.Common.Entities;
+
+namespace FlipNow.Business.Models;
+
+public class Player
+{
+    public Player(User user, ActiveGame game)
+    {
+        User = user;
+        Game = game;
+    }
+
+    public User User { get; }
+    public ActiveGame Game { get; }
+    public double Score { get; set; }
+    public int CardMatches { get; set; }
+    public int CardMatchesLeft => Game.Cards.Count / 2 - CardMatches;
+    public bool Finished { get; set; }
+    public TimeSpan TimeSpent { get; set; }
+}
