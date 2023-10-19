52cdf238e715f007d98e54bde65127d89f9661eb
Daniel Simonsen <danielsimonsen90@gmail.com>
2023-10-19 14:31:16 +0200

    Create ActiveGame.cs + FlipNowHelper.cs

---
+++ Projects/Backend/FlipNow.Business/FlipNowHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace FlipNow.Business;
+
+public static class FlipNowHelper
+{
+    public static string EnsureSlash(string value, bool start) =>
+        start
+            ? value.StartsWith('/') ? value : $"/{value}"
+            : value.EndsWith('/') ? value : $"{value}/";
+}
---
+++ Projects/Backend/FlipNow.Business/Models/ActiveGame.cs
@@ -0,0 +1,31 @@
+﻿using FlipNow.Common.Entities;
+
+namespace FlipNow.Business.Models;
+
+public class ActiveGame
+{
+    public ActiveGame(string invitePrefix, IEnumerable<Card> cards, IEnumerable<Player> players, Player host)
+    {
+        _invitePrefix = invitePrefix;
+        Cards = cards.Select(c => new GameCard(c)).ToList();
+        Players = players.ToList();
+        Host = host;
+    }
+
+    #region Invite
+    private readonly string _invitePrefix;
+    public string InviteCode { get; set; } = Guid.NewGuid().ToString();
+    public string InviteUrl => FlipNowHelper.EnsureSlash(_invitePrefix, start: false) + InviteCode;
+    #endregion
+
+    #region Players
+    public List<Player> Players { get; }
+    public int TurnPlayerIndex { get; set; } = 0;
+    public Player TurnPlayer => Players[TurnPlayerIndex];
+    public Player Host { get; }
+    public List<Player> Leaderboard => Players.OrderByDescending(p => p.Score).ToList();
+    #endregion
+
+    public List<GameCard> Cards { get; }
+    public PlayState PlayState { get; set; } = PlayState.IDLE;
+}
