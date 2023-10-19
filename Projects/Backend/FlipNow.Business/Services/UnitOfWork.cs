d03871c0c527a317ca40dd9af2ad77e1fc01791c
Daniel Simonsen <danielsimonsen90@gmail.com>
2023-10-19 14:13:42 +0200

    Create UnitOfWork

---
+++ Projects/Backend/FlipNow.Business/Services/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using DanhoLibrary.NLayer;
+using FlipNow.DataAccess;
+using FlipNow.DataAccess.Repositories;
+
+namespace FlipNow.Business.Services;
+
+public class UnitOfWork : BaseUnitOfWork<FlipNowDbContext>
+{
+    public UnitOfWork(FlipNowDbContext context) : base(context)
+    {
+        CardRepository = new(context);
+        GameRepository = new(context);
+        UserRepository = new(context);
+        UserScoreRepository = new(context);
+    }
+
+    public CardRepository CardRepository { get; set; }
+    public GameRepository GameRepository { get; set; }
+    public UserRepository UserRepository { get; set; }
+    public UserScoreRepository UserScoreRepository { get; set; }
+}
