ca8c8bccf951406fabc5205857d2785499dbab5c
Daniel Simonsen <danielsimonsen90@gmail.com>
2023-10-20 12:04:34 +0200

    Create blueprint for UserController

---
+++ Projects/Backend/FlipNow.API/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using FlipNow.Business.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlipNow.API.Controllers
+{
+    public class UsersController : BaseController
+    {
+        public UsersController(UnitOfWork unitOfWork) : base(unitOfWork) { }
+
+        [HttpPost("{username}")]
+        public IActionResult CreateOrFindUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{userId:Guid}")]
+        public IActionResult DeleteUser(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
