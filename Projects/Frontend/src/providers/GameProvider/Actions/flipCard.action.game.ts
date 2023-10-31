70849d3a6535b8bdfda25236bf9e8794670541fc
DanielSimonsen90 <danielsimonsen90@gmail.com>
2023-10-31 20:05:03 +0100

    Finish actions rewrite

---
+++ Projects/Frontend/src/providers/GameProvider/Actions/flipCard.action.game.ts
@@ -0,0 +1,7 @@
+import CreateGameAction from "./_CreateGameAction";
+
+export default CreateGameAction('flipCard', async ({ game, user, broadcastToHub, args: [cardIndex] }) => {
+  if (game.turnPlayer.user.id !== user.id) throw new Error('You have to wait your turn!');
+
+  broadcastToHub(cardIndex);
+});
