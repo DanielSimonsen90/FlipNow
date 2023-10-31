3f137b00431975e41c35b0ff9f420127ec969b14
Daniel Simonsen <danielsimonsen90@gmail.com>
2023-10-31 07:43:58 +0100

    Update game state on user dependency

--- Projects/Frontend/src/providers/GameProvider/GameProviderHooks.ts
+++ Projects/Frontend/src/providers/GameProvider/GameProviderHooks.ts
@@ -5,15 +5,23 @@ import { GameProviderContextType } from "./GameProviderTypes";
 import { Request } from "utils";
 import { ActiveGame } from "models/backend";
 import { Nullable } from "types";
+import { useAsyncEffect } from "danholibraryrjs";
 
 export const useGame = () => useContext(GameProviderContext);
 
-export async function useGetActiveGame(game: Nullable<ActiveGame>, setGame: Dispatch<SetStateAction<GameProviderContextType['game']>>) {
+export async function useGetActiveGame(
+  game: Nullable<ActiveGame>,
+  setGame: Dispatch<SetStateAction<GameProviderContextType['game']>>
+) {
   const { user } = useUser();
-  if (game || !user) return;
 
-  const response = await Request<ActiveGame, string>(`games?userId=${user.id}`);
-  if (!response.success) return console.error(response.text);
+  useAsyncEffect(async () => {
+    if (game && !user) return setGame(null); // User logged out or already has a game
+    if (game || !user) return; // User already has a game or hasn't logged in
 
-  setGame(response.data);
+    const response = await Request<ActiveGame, string>(`games?userId=${user.id}`);
+    if (!response.success) return console.error(response.text);
+
+    setGame(response.data);
+  }, [user]);
 }
