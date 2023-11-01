7d0c07260aeee0bc91f7423e367ab840b07e422f
Daniel Simonsen <danielsimonsen90@gmail.com>
2023-11-01 13:49:36 +0100

    Handle client unintended disconnect

--- Projects/Backend/FlipNow.Business/Services/SessionService.cs
+++ Projects/Backend/FlipNow.Business/Services/SessionService.cs
@@ -6,11 +6,15 @@ namespace FlipNow.Business.Services;
 public class GameSessionService
 {
     public readonly Dictionary<Guid, ActiveGame> HostedGames = new();
+    public readonly Dictionary<string, Guid> ConnectedUsers = new();
     public ActiveGame? FindActiveGame(string invideCode) => HostedGames.FirstOrDefault(kvp => kvp.Value.InviteCode == invideCode).Value;
     public ActiveGame? FindGameFromUser(User user) => HostedGames.FirstOrDefault(kvp => kvp.Value.Players.Any(p => p.User.Id == user.Id)).Value;
+    public ActiveGame? FindGameFromUserId(Guid userId) => HostedGames.FirstOrDefault(kvp => kvp.Value.Players.Any(p => p.User.Id == userId)).Value;
 
     public void AddGame(Guid hostId, ActiveGame game) => HostedGames.Add(hostId, game);
     public bool HasGame(Guid userId) => HostedGames.ContainsKey(userId);
     public void Update(Guid hostId, ActiveGame game) => HostedGames[hostId] = game;
     public void RemoveGame(Guid hostId) => HostedGames.Remove(hostId);
+
+    public Guid GetUserIdFromConnectionId(string connectionId) => ConnectedUsers.GetValueOrDefault(connectionId);
 }
--- Projects/Frontend/src/components/pages/Home/Buttons/DeleteGameButton/DeleteGameButton.tsx
+++ Projects/Frontend/src/components/pages/Home/Buttons/DeleteGameButton/DeleteGameButton.tsx
@@ -2,7 +2,7 @@ import { Button } from "danholibraryrjs";
 import { useGame } from "providers/GameProvider";
 
 export default function DeleteGameButton() {
-  const { dispatch } = useGame(false);
+  const { isHost, dispatch } = useGame(false);
 
   const onClick = () => {
     if (window.confirm(`Are you sure you want to delete your game?`)) {
@@ -10,7 +10,5 @@ export default function DeleteGameButton() {
     }
   };
 
-  return (
-    <Button importance="secondary" onClick={onClick}>Delete game</Button>
-  );
+  return isHost ? <Button importance="secondary" onClick={onClick}>Delete game</Button> : null;
 }
