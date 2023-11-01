0308235face9b03692e907cf4320a88baaae551c
Daniel Simonsen <danielsimonsen90@gmail.com>
2023-11-01 12:04:37 +0100

    Remove todo  comment

--- Projects/Frontend/src/providers/GameProvider/GameProviderConstants.ts
+++ Projects/Frontend/src/providers/GameProvider/GameProviderConstants.ts
@@ -2,7 +2,6 @@ import { createContext } from "react";
 
 import { GameProviderContextType } from "./GameProviderTypes";
 
-// TODO: Provide Player object to GameProviderContext
 export const GameProviderContext = createContext<GameProviderContextType>({
   game: null,
   isClientTurn: false,
