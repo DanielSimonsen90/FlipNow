a07fc4c92da1cfc1a45920e3e9200beb32b95a81
DanielSimonsen90 <danielsimonsen90@gmail.com>
2023-11-03 20:57:36 +0100

    Fix login-container not aligning to right when user is logged in

--- Projects/Backend/FlipNow.Business/Services/SessionService.cs
+++ Projects/Backend/FlipNow.Business/Services/SessionService.cs
@@ -7,17 +7,26 @@ public class GameSessionService
 {
     public readonly Dictionary<Guid, ActiveGame> HostedGames = new();
     public readonly Dictionary<string, Guid> ConnectedUsers = new();
+
+    #region Find game
     public ActiveGame? FindActiveGame(string invideCode) => HostedGames.FirstOrDefault(kvp => kvp.Value.InviteCode == invideCode).Value;
     public ActiveGame? FindGameFromUser(User user) => FindGameFromUserId(user.Id);
     public ActiveGame? FindGameFromUserId(Guid userId) => HostedGames.FirstOrDefault(kvp => 
         kvp.Value.Players.Any(p => p.User.Id == userId)
         || kvp.Value.Host.User.Id == userId
     ).Value;
+    #endregion
 
+    #region HostedGame CRUD
     public void AddGame(Guid hostId, ActiveGame game) => HostedGames.Add(hostId, game);
     public bool HasGame(Guid userId) => HostedGames.ContainsKey(userId);
     public void Update(Guid hostId, ActiveGame game) => HostedGames[hostId] = game;
     public void RemoveGame(Guid hostId) => HostedGames.Remove(hostId);
-
+    #endregion
+    
+    #region ConnectedUsers CRUD
     public Guid GetUserIdFromConnectionId(string connectionId) => ConnectedUsers.GetValueOrDefault(connectionId);
+    public void AddUserConnection(string connectionId, Guid userId) => ConnectedUsers.Add(connectionId, userId);
+    public void RemoveUserConnection(string connectionId) => ConnectedUsers.Remove(connectionId);
+    #endregion
 }
