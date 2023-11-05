50ce6568c6177256698321fbfd2a6f2f035c50fc
DanielSimonsen90 <danielsimonsen90@gmail.com>
2023-11-05 10:16:44 +0100

    GameSettings models

---
+++ Projects/Backend/FlipNow.Business/Models/GameSettings.cs
@@ -0,0 +1,10 @@
+﻿#nullable disable
+
+namespace FlipNow.Business.Models;
+
+public class GameSettings
+{
+    public int LobbyLimit { get; set; }
+    public string LobbyName { get; set; }
+    public int Cards { get; set; }
+}
