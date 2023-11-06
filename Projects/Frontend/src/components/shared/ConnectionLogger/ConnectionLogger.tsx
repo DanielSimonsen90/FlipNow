b7ced4779d04cc892ff873028a99171f7cc1bfc5
DanielSimonsen90 <danielsimonsen90@gmail.com>
2023-11-06 19:39:20 +0100

    Forgotten changes after merge error

--- Projects/Frontend/src/components/shared/ConnectionLogger/ConnectionLogger.tsx
+++ Projects/Frontend/src/components/shared/ConnectionLogger/ConnectionLogger.tsx
@@ -2,14 +2,12 @@ import { useState } from "react";
 import { Button } from "danholibraryrjs";
 
 import { useLogs } from "providers/ConnectionHubProvider";
-import { useGame } from "providers/GameProvider";
 
 import { ToggleButton } from "components/pages/Home/Buttons";
 
 export default function ConnectionLogger() {
-  const { isHost } = useGame();
   const { logs, setLogs } = useLogs();
-  const [showLogs, setShowLogs] = useState(isHost);
+  const [showLogs, setShowLogs] = useState(false);
 
   return (
     <div className="connection-logger">
